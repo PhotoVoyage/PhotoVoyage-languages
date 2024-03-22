2151813f31968ae774f1d6b98957ca80a59db297
akit <149289426+Sstudiosdev@users.noreply.github.com>
2024-03-22 15:53:03 -0600

    New commit App

--- app/menu.py
+++ app/menu.py
@@ -3,18 +3,17 @@ from PyQt5.QtWidgets import QAction, QMenu
 def create_menu(parent):
     menu_bar = parent.menuBar()
     
-    # File menu
-    file_menu = menu_bar.addMenu('&File')
-    new_action = QAction('&New', parent)
-    file_menu.addAction(new_action)
+    # Settings
+    file_menu = menu_bar.addMenu('&Settings')
+    exit_action = QAction('&Exit', parent)
+    exit_action.triggered.connect(parent.close)  # Connect the action to the parent's close function
+    file_menu.addAction(exit_action)
     
-    # Edit menu
-    edit_menu = menu_bar.addMenu('&Edit')
-    cut_action = QAction('&Cut', parent)
-    edit_menu.addAction(cut_action)
-    copy_action = QAction('&Copy', parent)
-    edit_menu.addAction(copy_action)
-    paste_action = QAction('&Paste', parent)
-    edit_menu.addAction(paste_action)
+    # languages menu
+    settings_menu = menu_bar.addMenu('&languages')
+    download_language_action = QAction('&Download Language', parent)
+    settings_menu.addAction(download_language_action)
+    credits_action = QAction('&Credits', parent)
+    settings_menu.addAction(credits_action)
     
     return menu_bar
