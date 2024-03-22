b03361f68ef7fd34a1662ba61de46d8a5316c3ac
akit <149289426+Sstudiosdev@users.noreply.github.com>
2024-03-22 16:44:30 -0600

    Update menu.py

--- app/menu.py
+++ app/menu.py
@@ -1,5 +1,6 @@
 from PyQt5.QtWidgets import QAction, QMenu
 from credits_window import CreditsWindow
+from language_download_window import LanguageDownloadWindow
 
 def create_menu(parent):
     menu_bar = parent.menuBar()
@@ -12,7 +13,10 @@ def create_menu(parent):
     
     # Languages menu (renamed from Help)
     settings_menu = menu_bar.addMenu('&Languages')
+
+    # Download Language action
     download_language_action = QAction('&Download Language', parent)
+    download_language_action.triggered.connect(lambda: show_language_download(parent))
     settings_menu.addAction(download_language_action)
     
     # Credits action
@@ -25,3 +29,7 @@ def create_menu(parent):
 def show_credits(parent):
     credits_window = CreditsWindow("md/credits.md")
     credits_window.exec_()
+
+def show_language_download(parent):
+    language_download_window = LanguageDownloadWindow()
+    language_download_window.exec_()
