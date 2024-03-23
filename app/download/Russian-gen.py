9898f4ec26b2dfd63acb3a8078522d1366b4cc98
akit <149289426+Sstudiosdev@users.noreply.github.com>
2024-03-22 23:21:15 -0600

    Update Russian-gen.py

--- app/download/Russian-gen.py
+++ app/download/Russian-gen.py
@@ -2,6 +2,7 @@
 
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QListWidget
+from PyQt5.QtGui import QIcon
 
 class RussianGenerator(QWidget):
     def __init__(self):
@@ -9,6 +10,8 @@ class RussianGenerator(QWidget):
 
         self.setWindowTitle("Русская загрузка")
         self.setGeometry(100, 100, 300, 400)
+        icon = QIcon("ico/161897722.ico")
+        self.setWindowIcon(icon)
 
         layout = QVBoxLayout()
 
